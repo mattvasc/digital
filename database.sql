4420a7829aaef82e344a2715061f64a547cf2931
Mateus S. Vasconcelos <mateus.s.vasconcelos@outlook.com>
2018-06-11 20:27:00 -0300

    Add files via upload

---
+++ database.sql
@@ -0,0 +1,9 @@
+PRAGMA foreign_keys = ON;
+PRAGMA encoding="UTF-8";
+
+CREATE TABLE `user` (`id` INTEGER PRIMARY KEY AUTOINCREMENT,  `name` varchar(128) NOT NULL,   `email` varchar(256) NOT NULL,   `phone` varchar(64));
+CREATE TABLE `log` ( `id` INTEGER PRIMARY KEY AUTOINCREMENT,  `userid` int(10) unsigned NOT NULL,  `date` datetime NOT NULL, FOREIGN KEY (userid) REFERENCES user(id));
+CREATE TABLE `fingerprints` (`id` INTEGER PRIMARY KEY AUTOINCREMENT, `userid` INTEGER  ,  `fingerprint_id` INTEGER  ,  `finger` INTEGER,  FOREIGN KEY (userid) REFERENCES user(id));
+
+
+
